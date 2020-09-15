fa784955f12d20941d14291e5de31947a7201635
kriyanation <homeofmars@gmail.com>
2020-09-15 08:40:59 +0530

    Update test_google.py
    check

--- sources/test_google.py
+++ sources/test_google.py
@@ -5,17 +5,7 @@ from googleapiclient.discovery import build
 import requests
 
 headers = {'Content-Type': 'application/json'}
-url = "https://customsearch.googleapis.com/customsearch/v1?cx=dc036f5cac32deb3d&imgSize=LARGE&num=10&q=friction&safe=active&key=AIzaSyAgTTei1O1_b1DTvrJWbEbM8tuyE_Fm1iA"
 
-
-try:
-    service = build("customsearch", "v1",
-                    developerKey="AIzaSyAgTTei1O1_b1DTvrJWbEbM8tuyE_Fm1iA")
-    res = service.cse().list(
-        q='friction',
-        cx='dc036f5cac32deb3d',
-        num=10
-    ).execute()
 
     for element in res['items']:
         print(element['pagemap']['cse_image'][0]['src'])
